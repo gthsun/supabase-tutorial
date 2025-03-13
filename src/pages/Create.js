87a2304d4bc5e6609f4d0e10c0e2a9922b9beb1e
gthsun <sundaram.geethan@gmail.com>
2025-03-13 12:25:36 -0400

    Added ability to add games

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,71 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+
 const Create = () => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [rating, setRating] = useState("");
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !desc || !rating) {
+      setFormError("Please fill in all the fields correctly.");
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("games")
+      .insert([{ title, desc, rating }])
+      .select();
+
+    if (error) {
+      console.log(error);
+      setFormError("Please fill in all the fields correctly.");
+    }
+    if (data) {
+      console.log(data);
+      setFormError(null);
+      navigate("/");
+    }
+  };
+
   return (
     <div className="page create">
-      <h2>Create</h2>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-export default Create
+        <label htmlFor="desc">Description:</label>
+        <textarea
+          id="desc"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Store Game</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Create;
