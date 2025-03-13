4517d63e01e943aaea898e02db3029f0cbb25fd5
gthsun <sundaram.geethan@gmail.com>
2025-03-13 12:42:58 -0400

    Fetched records from supabase for update page

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,11 +1,18 @@
-const GameCard = ({ game }) => {
-    return (
-        <div className="game-card">
-            <h3>{game.title}</h3>
-            <p>{game.desc}</p>
-            <div className="rating">{game.rating}</div>
-        </div> 
-    )
-}
+import { Link } from "react-router-dom";
 
-export default GameCard
+const GameCard = ({ game }) => {
+  return (
+    <div className="game-card">
+      <h3>{game.title}</h3>
+      <p>{game.desc}</p>
+      <div className="rating">{game.rating}</div>
+      <div className="buttons">
+        <Link to={'/' + game.id}>
+            <i className="material-icons">edit</i>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,69 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    const fetchGame = async () => {
+      const { data, error } = await supabase
+        .from("games")
+        .select()
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        navigate("/", { replace: true });
+      }
+      if (data) {
+        setTitle(data.title);
+        setDesc(data.desc);
+        setRating(data.rating);
+        console.log(data);
+      }
+    };
+
+    fetchGame();
+  }, [id]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
-}
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-export default Update
+        <label htmlFor="desc">Description:</label>
+        <textarea
+          id="desc"
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Game</button>
+      
+        {/*formError && <p className="error">{formError}</p>*/}
+      </form>
+    </div>
+  );
+};
+
+export default Update;
